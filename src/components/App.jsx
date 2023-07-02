ae208a72793ade612d4d47962e9f1d298d3274ea
Ayk <brutalmind99@gmail.com>
2023-07-02 12:50:33 +0800

    latest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,49 +2,43 @@ import React, { useState } from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
 import CreateArea from "./CreateArea";
 
 function App() {
-    const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-    function addNote(newNote){
-        setNotes((prevNotes) => {
-        return [...prevNotes, newNote];
-        });
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    }
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    function deleteNote(id) {
-        setNotes(prevNotes => {
-          return prevNotes.filter((noteItem, index)=> {
-                return index !== id;
-            });
-        });
-    }
-
-    return (
-        <div>
-        <Heading />
-        <CreateArea 
-            onAdd={addNote}
-        />
-     { notes.map((noteItem, index) => (
-        <Note   
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
-            onDelete={deleteNote}
+  return (
+    <div>
+      <Heading />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
         />
       ))}
-        
-       
-        <Footer />
-        
-    </div>
-    ); 
-}
 
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,58 +1,85 @@
 import React, { useState } from "react";
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab';
-import Zoom from '@mui/material/Zoom';
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 const add = {
-    "&:hover": {
-        color:"#fff",
-        backgroundColor: "#616161"
-    }
-}
+  "&:hover": {
+    color: "#fff",
+    backgroundColor: "#616161",
+  },
+};
 
 function CreateArea(props) {
-    const [isExpanded, setExpanded] = useState(false);
-    const [note, setNote] = useState({
-        title:"",
-        content:""
+  const [isExpanded, setExpanded] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
     });
-    function handleChange(event) {
-        const {name, value} = event.target;
-        setNote((prevNote) => {
-            return {
-                ...prevNote,
-                [name]: value
-            };
-        });
-    }
+  }
 
-    function submitNote(event){
-        //form is automatic reload this code prevent reload
-        props.onAdd(note);
-        event.preventDefault();
-        setNote({
-            title:"",
-            content: ""
-        });
-    }
+  function submitNote(event) {
+    //form is automatic reload this code prevent reload
+    props.onAdd(note);
+    event.preventDefault();
+    setNote({
+      title: "",
+      content: "",
+    });
+  }
 
-    function expanded() {
-        setExpanded(true)
-    }
+  function expanded() {
+    setExpanded(true);
+  }
 
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+            value={note.title}
+          />
+        )}
 
-    return (
-        <div>
-            <form className="create-note">
-                {isExpanded &&
-                <input onChange={handleChange} name="title" placeholder="Title" value={note.title}/> }
-
-
-                <textarea onClick={expanded} onChange={handleChange} name="content" placeholder="Take a note..." rows={isExpanded ? 3 :1} value={note.content}/> 
-                <Zoom sx={add} in={isExpanded}><Fab onClick={submitNote}><AddIcon /></Fab></Zoom>
-            </form>
-        </div>
-    )
+        <textarea
+          onClick={expanded}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+          value={note.content}
+        />
+        <Zoom sx={add} in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
+
+// import "./AddTask.css";
+
+// import React from "react";
+
+// export const AddTask = () => {
+//   return (
+//     <section className="addtask">
+//       <form action=""></form>
+//     </section>
+//   );
+// };
